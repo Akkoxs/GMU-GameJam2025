20ea404894ad1ba4245575517227811272b583e4
ZeinAlbahrani <57455654+ZeinAlbahrani@users.noreply.github.com>
2025-01-18 15:16:55 -0500

    Added player prefab with movement

--- JammyJam2025/Assets/Player.cs
+++ JammyJam2025/Assets/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed;
     float gravity = -20;
     Vector3 velocity;
 
@@ -9,13 +10,16 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        velocity.y += gravity + Time.deltaTime;
-        //controller.Move(velocity * Time.deltaTime); i commented this out casue it was causing compilation issue
+        controller = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 input = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity + Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
--- JammyJam2025/Assets/PlayerController.cs
+++ JammyJam2025/Assets/PlayerController.cs
@@ -14,21 +14,31 @@ public class PlayerController : MonoBehaviour
     float horizontalRaySpacing;
     float verticalRaySpacing;
 
+    public CollisionInfo collisionInfo;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         pCollider = GetComponent<BoxCollider2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateRaycastOrigins();
         CalculateRaySpacing();
     }
-    
-    void Move()
+
+    public void Move(Vector3 velocity)
     {
+        UpdateRaycastOrigins();
+        collisionInfo.Reset();
+
+        if (velocity.x != 0)
+        {
+            HorizontalCollision(ref velocity);
+        }
+
+        if (velocity.y != 0)
+        {
+            VerticalCollisions(ref velocity);
+        }
+
+        transform.Translate(velocity);
     }
 
     void VerticalCollisions(ref Vector3 velocity)
@@ -42,14 +52,34 @@ public class PlayerController : MonoBehaviour
             rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
 
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
             if (hit)
             {
                 velocity.y = (hit.distance - skinWidth) * directionY;
                 rayLength = hit.distance;
             }
         }
+    }
 
+    void HorizontalCollision(ref Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
 
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.x = (hit.distance - skinWidth) * directionX;
+                rayLength = hit.distance;
+            }
+        }
     }
 
     void UpdateRaycastOrigins()
@@ -71,13 +101,24 @@ public class PlayerController : MonoBehaviour
         horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
         verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
 
-        horizontalRaySpacing = bounds.size.y / ( horizontalRaySpacing - 1);
-        verticalRaySpacing = bounds.size.x / ( verticalRaySpacing - 1);
+        horizontalRaySpacing = bounds.size.y / (horizontalRaySpacing - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRaySpacing - 1);
     }
-    
+
     struct RaycastOrigins
     {
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+
+        public void Reset()
+        {
+            above = false; below = false; left = false; right = false;
+        }
     }
 }
