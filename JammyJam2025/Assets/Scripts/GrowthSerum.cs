9c203c7e8ff44c3dbbae7761f1d1c12b4797ae18
Kai <kai.stewart.eng@gmail.com>
2025-01-26 03:18:42 -0500

    serum random spawning + serum animation + serum inventory

--- JammyJam2025/Assets/Scripts/GrowthSerum.cs
+++ JammyJam2025/Assets/Scripts/GrowthSerum.cs
@@ -1,25 +1,44 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GrowthSerum : MonoBehaviour
 {
     public bool pickedUpSerum;
     public bool droppedOffSerum;
+    public int sidePicker;
+    public float LHSnewPos;
+    public float RHSnewPos;
     public GameObject baseMushroomTrigger;
+
+    private Transform trans;
     private SpriteRenderer spriteRenderer;
+
+    [SerializeField] public Image inventorySerum; 
+
+    [SerializeField] public float LHS_maxSpawn;
+    [SerializeField] public float LHS_minSpawn;
+    [SerializeField] public float RHS_minSpawn;
+    [SerializeField] public float RHS_maxSpawn;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         droppedOffSerum = false;
+        inventorySerum.enabled = false; 
         spriteRenderer = GetComponent<SpriteRenderer>();
+        trans = GetComponent<Transform>();
     }
 
-    private void Update()
+    public void Update()
     {
         if (droppedOffSerum)
         {
+            inventorySerum.enabled = false;
+            sidePicker = Random.Range(1,3);
+            LHSnewPos = Random.Range(LHS_minSpawn, LHS_maxSpawn);
+            RHSnewPos = Random.Range(RHS_minSpawn, RHS_maxSpawn);
             StartCoroutine(Wait());
         }
     }
@@ -29,6 +48,7 @@ public class GrowthSerum : MonoBehaviour
         if (collision.CompareTag("Player") && !pickedUpSerum)
         {
             pickedUpSerum = true;
+            inventorySerum.enabled = true;
             baseMushroomTrigger.SetActive(true);
             spriteRenderer.enabled = false;
         }
@@ -37,9 +57,21 @@ public class GrowthSerum : MonoBehaviour
     public IEnumerator Wait()
     {
         yield return new WaitForSecondsRealtime(5f);
+        RandomSpawner();
         pickedUpSerum = false;
         droppedOffSerum = false;
         baseMushroomTrigger.SetActive(false);
         spriteRenderer.enabled = true;
     }
+
+    public void RandomSpawner(){
+        if (sidePicker == 1){ //LHS
+            transform.position = new Vector2(LHSnewPos, trans.position.y);
+        }
+
+        else if (sidePicker == 2){ //RHS
+            transform.position = new Vector2(RHSnewPos, trans.position.y);
+        }
+    }
+
 }
