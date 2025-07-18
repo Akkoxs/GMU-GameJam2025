4807eb5c43f75b65fbdb078c692092808605fe15
Kai <kai.stewart.eng@gmail.com>
2025-07-18 18:49:58 -0400

    Still workin on main menu

--- JammyJam2025/Assets/UI/MainMenu.cs
+++ JammyJam2025/Assets/UI/MainMenu.cs
@@ -7,12 +7,16 @@
     {
 
         [SerializeField] private RawImage BlackScreen;
-        [SerializeField] private Transform PlayerSprite;
+        [SerializeField] private Transform playerSprite;
         [SerializeField] private Transform CenterPoint;
 
         static Color BlFade;
 
         private float blackScreenTime = 1.3f;
+        private float moveIncrement = 0.00001f;
+        private bool isFalling = false; 
+        private Vector3 p_pos;
+        private Vector3 cp;
 
         public void Start()
         {
@@ -23,17 +27,29 @@
         }
 
         public void Update(){
-            //Fallsway
+            playerSprite.position = p_pos;
+            if (!isFalling){
+                StartCoroutine(FallSway());
+                isFalling = true;
+            }
         }
 
+        //while true, sway
+        //maybe have a lerp movment to a new Y
+        //new Y is calculated after place is reached as a RAND between 2 limits
         private IEnumerator FallSway(){
-            yield return new WaitForSecondsRealtime(blackScreenTime);
-            //while we are not at target height, fall
-
-            //while true, sway
-                //maybe have a lerp movment to a new Y
-                //new Y is calculated after place is reached as a RAND between 2 limits
-            yield return null; 
+            //elapsed = 0;
+            while(p_pos.y > cp.y){
+                p_pos.y -= moveIncrement;
+                if(p_pos.y > cp.y){
+                    p_pos.y = cp.y;
+                }
+                yield return null;
+            }
+            while(true){
+                //p_pos.y = playerSprite.position.y;
+                yield return null; 
+            }
         }
 
     public IEnumerator FadeScreen(Color UI, float start, float end, float duration, float delay, float endDelay, bool isActive, bool endScene)
